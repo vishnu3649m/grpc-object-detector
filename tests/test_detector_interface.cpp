e561b8931ca0e72e8495e4ec7c6cde9b636dd594
Nachi <nachi@dsaid.gov.sg>
2021-02-28 10:59:41 +0800

    Add more tests for what a detector should do
    * cleaned up unnecessary images because I wanted to keep things
      simple at first.

--- tests/test_detector_interface.cpp
+++ tests/test_detector_interface.cpp
@@ -57,17 +57,11 @@ private:
 class DetectorInterfaceTest : public ::testing::Test {
 protected:
     VA::DetectorInterface *detector;
-    vector<VA::Detection> detections_1;
-    vector<VA::Detection> detections_2;
-    vector<VA::Detection> detections_3;
-    vector<VA::Detection> detections_4;
+    vector<VA::Detection> detections;
 
     DetectorInterfaceTest() {
         detector = new DummyConcreteDetector();
-        detections_1 = detector->detect(cv::imread("tests/data/1.jpg"));
-        detections_2 = detector->detect(cv::imread("tests/data/2.jpg"));
-        detections_3 = detector->detect(cv::imread("tests/data/3.jpg"));
-        detections_4 = detector->detect(cv::imread("tests/data/4.jpg"));
+        detections = detector->detect(cv::imread("tests/data/1.jpg"));
     }
 
     ~DetectorInterfaceTest() override {
@@ -77,8 +71,44 @@ protected:
 
 
 TEST_F(DetectorInterfaceTest, ProducesDetections) {
-    ASSERT_GT(detections_1.size(), 0);
-    ASSERT_GT(detections_2.size(), 0);
-    ASSERT_GT(detections_3.size(), 0);
-    ASSERT_GT(detections_4.size(), 0);
+    ASSERT_GT(detections.size(), 0);
+}
+
+TEST_F(DetectorInterfaceTest, ProducesNormalizedBoundingBoxes) {
+    ASSERT_GT(detections.size(), 0);
+
+    for (auto & det : detections) {
+        EXPECT_GE(det.box.left, 0.0f);
+        EXPECT_LE(det.box.left, 1.0f);
+
+        EXPECT_GE(det.box.top, 0.0f);
+        EXPECT_LE(det.box.top, 1.0f);
+
+        EXPECT_GE(det.box.width, 0.0f);
+        EXPECT_LE(det.box.width, 1.0f);
+
+        EXPECT_GE(det.box.height, 0.0f);
+        EXPECT_LE(det.box.height, 1.0f);
+    }
+}
+
+TEST_F(DetectorInterfaceTest, ProducesConfidenceScoresForEachDetection) {
+    ASSERT_GT(detections.size(), 0);
+
+    for (auto & det : detections) {
+        EXPECT_GE(det.confidence, 0.0f);
+        EXPECT_LE(det.confidence, 1.0f);
+    }
+}
+
+TEST_F(DetectorInterfaceTest, MapsAllDetectedObjectClassIdsToHumanReadableString) {
+    ASSERT_GT(detections.size(), 0);
+
+    for (auto & det : detections) {
+        int cls_id = det.class_id;
+        EXPECT_GE(cls_id, 0);
+        EXPECT_LE(cls_id, 9);
+        EXPECT_EQ(detector->class_id_to_label(cls_id),
+                  absl::StrFormat("class_%d", cls_id));
+    }
 }
