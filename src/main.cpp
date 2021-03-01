a05509a007bd6591ff45d350d62734f218935900
Nachi <nachi@dsaid.gov.sg>
2021-03-02 07:50:27 +0800

    Add skeleton for starting server via CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 
-#include <loguru/loguru.hpp>
+#include <loguru.hpp>
+#include <CLI/CLI.hpp>
+#include <absl/strings/str_format.h>
+
+using namespace std;
+
+#define VIDEO_ANALYZER_VERSION_MAJOR 0
+#define VIDEO_ANALYZER_VERSION_MINOR 1
+#define VIDEO_ANALYZER_VERSION_PATCH 0
 
 int main(int argc, char **argv) {
+    CLI::App app{"gRPC Video Analyzer"};
+
+    bool version = false;
+    string action;
+
+    app.add_flag("--version,-V", version, "Prints version info and exits");
+    app.add_option("action", action, "Server action to perform")->check(CLI::IsMember({"start"}));
+
+    CLI11_PARSE(app, argc, argv);
+
+    if (version) {
+        string version_msg = absl::StrFormat("gRPC Video Analyzer %d.%d.%d",
+                                             VIDEO_ANALYZER_VERSION_MAJOR,
+                                             VIDEO_ANALYZER_VERSION_MINOR,
+                                             VIDEO_ANALYZER_VERSION_PATCH);
+        cout << version_msg << endl;
+        return 0;
+    }
+
     loguru::init(argc, argv);
-    LOG_F(INFO, "Starting gRPC server...");
+
+    if (action == "start") {
+        LOG_F(INFO, "Starting gRPC server...");
+    }
+
     return 0;
 }
